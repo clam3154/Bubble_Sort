95ce5966e8347d5109bbbd5e90369f053c50a6aa
BT_1N3_06@bktech.local <BT_1N3_06@bktech.local>
2018-11-13 10:17:54 -0500

    Initial commit

---
+++ src/bubbleSort.java
@@ -0,0 +1,22 @@
+public class bubbleSort
+{
+    public void bubbleSort(int[] arr)
+    {
+        boolean check = false;
+        while(check = false)
+        {
+            check = true;
+            for(int i = 0; i < arr.length-1; i++)
+            {
+
+            }
+        }
+    }
+
+    public void swap(int[] array, int pos1, int pos2)
+    {
+        int temp = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = temp;
+    }
+}
